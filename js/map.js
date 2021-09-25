75cbf6f316df0d10e33a5d13213a2cfa2d4bc594
HikoHohodemiNoMikoto <89847796+HikoHohodemiNoMikoto@users.noreply.github.com>
2021-09-25 11:35:09 +0300

    indeex.html commit mb fuul
    почти фул

---
+++ js/map.js
@@ -0,0 +1,42 @@
+JAVASCRIPT STYLE ARRAY:
+    [
+        {
+            "featureType": "all",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "lightness": "50"
+                },
+                {
+                    "saturation": "0"
+                },
+                {
+                    "gamma": "1"
+                }
+            ]
+        },
+        {
+            "featureType": "all",
+            "elementType": "labels",
+            "stylers": [
+                {
+                    "lightness": "25"
+                },
+                {
+                    "saturation": "0"
+                },
+                {
+                    "visibility": "on"
+                }
+            ]
+        },
+        {
+            "featureType": "poi.business",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        }
+    ]1
---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $('.slider').slick({
+        arrows: false,
+        dots: true,
+        adaptiveHeight: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 1000,
+        easeng: 'ease',
+        infinite: true,
+        instalSlide: 0,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        pauseOnFocus: true,
+        pauseOnHover: true,
+        pauseOnDotsHover: true,
+
+    });
+});
